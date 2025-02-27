9e499bfc05bde4c083cb9e84e9ce584dd2b58f20
Your Name <you@example.com>
2025-02-27 15:46:40 +0700

    update code

--- BeautySoftBE/Models/RoleModel.cs
+++ BeautySoftBE/Models/RoleModel.cs
@@ -1,9 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace BeautySoftBE.Models
 {
 	public class RoleModel
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public ICollection<UserModel> Users { get; set; }
+		
+		[JsonIgnore]
+		public ICollection<UserModel?> Users { get; set; } = new List<UserModel?>();
 	}
 }
